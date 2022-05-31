521abae8686fac45467f84539d58dc9213f4582a
felix_blockchainer <97702158+flockchain@users.noreply.github.com>
2022-05-31 18:29:30 +0200

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+// Import Assets
+import profile from '../assets/profile.png';
+
+const About = () => {
+    return (
+        <section className='about'>
+            <h2>About Me</h2>
+            <p>
+                Here is a short introduction to myself. I am Felix Stefan Jost, 23 years old and currently live in the south of Germany. 
+                I am actually in the 4th semester of my computer science studies, and in parallel I am very much involved in blockchain technology. 
+                Starting with important use cases, over well-founded general knowledge about the most popular blockchains and their functionality, 
+                up to cryptographic algorithms and important programming languages like Solidity, I have acquired a lot of knowledge. 
+                Besides the many activities at my desk, I like to do sports, including power lifting and mixed martial arts. 
+                I am also a family man and a big football fan.
+            </p>
+        </section>
+    );
+}
+
+export default About;
